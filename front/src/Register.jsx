38f460767ed8556875c5050019ec43d69542554c
Dileesh sai <88364297+Dileeshsai@users.noreply.github.com>
2024-08-28 10:04:17 +0530

    Update Register.jsx

--- front/src/Register.jsx
+++ front/src/Register.jsx
@@ -13,7 +13,7 @@ function Register() {
 
   const handleSubmit = (e) => {
       e.preventDefault()
-      axios.post("http://localhost:3001/register", { name, email, password })
+      axios.post("https://react-h7en.vercel.app/register", { name, email, password })
       .then(result => {console.log(result)
       navigate("/loginForm")
       })
