f0c0cf8fcdd2f1fef4e8f151be4106c688ae79bb
Dileesh sai <88364297+Dileeshsai@users.noreply.github.com>
2024-08-30 10:56:48 +0530

    Update Register.jsx

--- front/src/Register.jsx
+++ front/src/Register.jsx
@@ -10,10 +10,10 @@ function Register() {
   const [email, setEmail] = useState()
   const [password, setPassword] = useState()
   const navigate = useNavigate()
-
+axios.defaults.withCredentials=true;
   const handleSubmit = (e) => {
       e.preventDefault()
-      axios.post("https://react-h7en.vercel.app/", { name, email, password })
+      axios.post("https://react-h7en.vercel.app/register", { name, email, password })
       .then(result => {console.log(result)
       navigate("/loginForm")
       })
